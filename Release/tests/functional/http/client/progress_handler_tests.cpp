06ede4575a25dc3b6af2dce4ca116d5df31f3707
Wolfgang Stöggl <c72578@yahoo.de>
2017-10-24 18:17:22 +0200

    Fix nested comment in progress_handler_tests.cpp

--- Release/tests/functional/http/client/progress_handler_tests.cpp
+++ Release/tests/functional/http/client/progress_handler_tests.cpp
@@ -355,7 +355,7 @@ TEST_FIXTURE(uri_address, download_nobody_exception)
     VERIFY_THROWS(client.request(msg).get().content_ready().get(), std::invalid_argument);
 }
 
-/*
+#if 0
 TEST_FIXTURE(uri_address, data_upload_exception)
 {
     http_client client(m_uri);
@@ -376,7 +376,7 @@ TEST_FIXTURE(uri_address, data_upload_exception)
     try { t.get(); }
     catch (const std::runtime_error&) { /* It is ok if the request does not complete before the server is shutdown */ }
 }
-*/
+#endif
 
 TEST_FIXTURE(uri_address, data_download_exception, "Ignore:Windows", "395")
 {
